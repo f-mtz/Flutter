9bf81d8f74e091bfac44846bd47d39e19f061255
SOLUTIS\felipe.ribeiro <felipe.ribeiro@solutis.com.br>
2021-04-21 18:35:21 -0300

    Stateless e Stateful

--- contador_de_pessoas_restaurant/lib/main.dart
+++ contador_de_pessoas_restaurant/lib/main.dart
@@ -3,42 +3,72 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Stack(
-        children: [
-          Image.asset("img/restaurant.jpg", fit: BoxFit.cover, height: 1000.0,),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Pessoas: 0",
-                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "-1",
-                        style: TextStyle(color: Colors.white, fontSize: 40),
-                      ), onPressed: () {},),),
-                  Padding(padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "-1",
-                        style: TextStyle(color: Colors.white, fontSize: 40),
-                      ), onPressed: () {},),)
-                ],
-              ),
-              Text(
-                "Pode entrar !",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 30.0),
-              )
-            ],
-          )
-        ],
-      )));
+      home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _pessoas = 0;
+  String _infoText = "Pode entrar !";
+  void _mudaPessoas(int delta){
+    setState(() {
+      _pessoas += delta;
+      if(_pessoas < 0) {
+        _infoText = "Mundo invertido";
+      }
+      else if(_pessoas <= 10) {
+        _infoText = "Pode Entrar!";
+      }
+      else {
+        _infoText = "Lotado";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset("img/restaurant.jpg", fit: BoxFit.cover, height: 1000.0,),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Pessoas: $_pessoas",
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ), onPressed: () {_mudaPessoas(1);},),),
+                Padding(padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ), onPressed: () {_mudaPessoas(-1);},),)
+              ],
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        )
+      ],
+    );
+  }
 }
