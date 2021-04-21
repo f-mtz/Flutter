e7065dcd599783490079fbb87025713207879478
SOLUTIS\felipe.ribeiro <felipe.ribeiro@solutis.com.br>
2021-04-21 17:00:24 -0300

    Botoes, linhas e Hierarquia

--- contador_de_pessoas_restaurant/lib/main.dart
+++ contador_de_pessoas_restaurant/lib/main.dart
@@ -10,13 +10,27 @@ void main() {
             "Pessoas: 0",
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FlatButton(
+                child: Text(
+                  "-1",
+                  style: TextStyle(color: Colors.pink, fontSize: 40),
+                ), onPressed: () {},),
+              FlatButton(
+                  child: Text(
+                    "+1",
+                    style: TextStyle(color: Colors.pink, fontSize: 40),
+                  ), onPressed: () {},),
+            ],
+          ),
           Text(
             "Pode entrar !",
             style: TextStyle(
-              color: Colors.white,
-              fontStyle: FontStyle.italic,
-              fontSize: 30.0
-            ),
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0),
           )
         ],
       )));
